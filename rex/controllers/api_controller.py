9f45c6bcb6f1149ee8537b97e4464b051a889716
Your Name <you@example.com>
2019-04-06 09:49:32 +0700

    a

--- rex/controllers/api_controller.py
+++ rex/controllers/api_controller.py
@@ -724,7 +724,11 @@ def upload_img_profile(customer_id):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    name = upload.filename
+
+    localtime = time.localtime(time.time())
+    str_string = '%s%s%s%s%s%s'%(localtime.tm_mon,localtime.tm_year,localtime.tm_mday,localtime.tm_hour,localtime.tm_min,localtime.tm_sec)
+    
+    name = str_string+'_'+upload.filename
     #print name,save_path
     file_path = "{path}/{file}".format(path=save_path, file=name)
     upload.save(file_path)
